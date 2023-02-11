14b4510d46ca938f907744550fc5042c7af402e6
Priyank Tejani <priyanktejani7@gmail.com>
2023-02-11 19:00:42 +0000

    Add company listing screen

---
+++ app/src/main/java/com/example/stockmarketapp/presentation/company_listings/CompanyListingScreen.kt
@@ -0,0 +1,79 @@
+package com.example.stockmarketapp.presentation.company_listings
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.Divider
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
+import com.google.accompanist.swiperefresh.SwipeRefresh
+import com.google.accompanist.swiperefresh.rememberSwipeRefreshState
+
+@ExperimentalMaterial3Api
+@Composable
+fun CompanyListingScreen(
+    viewModel: CompanyListingsViewModel = hiltViewModel()
+) {
+    val swipeRefreshState = rememberSwipeRefreshState(
+        isRefreshing = viewModel.state.refreshing,
+    )
+    val state = viewModel.state
+    Column(
+        modifier = Modifier.fillMaxSize()
+    ) {
+        OutlinedTextField(
+            value = state.searchQuery,
+            onValueChange = {
+                viewModel.onEvent(
+                    CompanyListingsEvent.OnSearchQueryChange(it)
+                )
+            },
+            modifier = Modifier
+                .padding(16.dp)
+                .fillMaxWidth(),
+            placeholder = {
+                Text(text = "Search...")
+            },
+            maxLines = 1,
+            singleLine = true
+        )
+        SwipeRefresh(
+            state = swipeRefreshState,
+            onRefresh = {
+                viewModel.onEvent(CompanyListingsEvent.Refresh)
+            }
+        ) {
+            LazyColumn(
+                modifier = Modifier.fillMaxSize()
+            ) {
+                items(state.companies.size) { i ->
+                    val company = state.companies[i]
+                    CompanyItem(
+                        company = company,
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .clickable {
+                                // Todo: Navigate to company detail screen
+                            }
+                            .padding(16.dp)
+                    )
+                    if(i < state.companies.size) {
+                        Divider(
+                            modifier = Modifier.padding(
+                            horizontal = 16.dp
+                            )
+                        )
+                    }
+                }
+            }
+        }
+    }
+}
